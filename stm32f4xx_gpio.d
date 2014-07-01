4e0c5ba70de23cba65663571d3f3820c264801e8
Demcsik Márton <demcsik@gmail.com>
2014-07-01 14:04:29 +0200

    Change

---
+++ stm32f4xx_gpio.d
@@ -0,0 +1,15 @@
+.\stm32f4xx_gpio.o: src\stm32f4xx_gpio.c
+.\stm32f4xx_gpio.o: .\inc\stm32f4xx_gpio.h
+.\stm32f4xx_gpio.o: .\inc\stm32f4xx.h
+.\stm32f4xx_gpio.o: .\inc\Core\core_cm4.h
+.\stm32f4xx_gpio.o: d:\Keil_v5\ARM\ARMCC\bin\..\include\stdint.h
+.\stm32f4xx_gpio.o: .\inc\Core\core_cmInstr.h
+.\stm32f4xx_gpio.o: .\inc\Core\core_cmFunc.h
+.\stm32f4xx_gpio.o: .\inc\Core\core_cm4_simd.h
+.\stm32f4xx_gpio.o: .\inc\system_stm32f4xx.h
+.\stm32f4xx_gpio.o: .\inc\stm32f4xx_conf.h
+.\stm32f4xx_gpio.o: .\inc\stm32f4xx_gpio.h
+.\stm32f4xx_gpio.o: .\inc\stm32f4xx_rcc.h
+.\stm32f4xx_gpio.o: .\inc\stm32f4xx.h
+.\stm32f4xx_gpio.o: .\inc\stm32f4xx_usart.h
+.\stm32f4xx_gpio.o: .\inc\misc.h
---
+++ stm32f4xx_init_rcc.d
@@ -0,0 +1,14 @@
+.\stm32f4xx_init_rcc.o: src\stm32f4xx_init_rcc.c
+.\stm32f4xx_init_rcc.o: .\inc\stm32f4xx.h
+.\stm32f4xx_init_rcc.o: .\inc\Core\core_cm4.h
+.\stm32f4xx_init_rcc.o: d:\Keil_v5\ARM\ARMCC\bin\..\include\stdint.h
+.\stm32f4xx_init_rcc.o: .\inc\Core\core_cmInstr.h
+.\stm32f4xx_init_rcc.o: .\inc\Core\core_cmFunc.h
+.\stm32f4xx_init_rcc.o: .\inc\Core\core_cm4_simd.h
+.\stm32f4xx_init_rcc.o: .\inc\system_stm32f4xx.h
+.\stm32f4xx_init_rcc.o: .\inc\stm32f4xx_conf.h
+.\stm32f4xx_init_rcc.o: .\inc\stm32f4xx_gpio.h
+.\stm32f4xx_init_rcc.o: .\inc\stm32f4xx.h
+.\stm32f4xx_init_rcc.o: .\inc\stm32f4xx_rcc.h
+.\stm32f4xx_init_rcc.o: .\inc\stm32f4xx_usart.h
+.\stm32f4xx_init_rcc.o: .\inc\misc.h
